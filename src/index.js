5deeaff595667d4c7d68e1b41f188f66bdffd981
Nishaanthini <nishaanthini14@gmail.com>
2024-09-02 18:15:21 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,234 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { IoMenuOutline } from "react-icons/io5";
+import { MdHomeFilled } from "react-icons/md";
+import { SiYoutubeshorts } from "react-icons/si";
+import { MdOutlineSubscriptions } from "react-icons/md";
+import { MdOutlineVideoLibrary } from "react-icons/md";
+import { HiOutlineDotsVertical } from "react-icons/hi";
+import { IoIosSearch } from "react-icons/io";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { GrYoutube } from "react-icons/gr";
+import image1 from './Assets/image1.png';
+import image2 from './Assets/image2.png';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Youtube(){
+  return(
+    <div className="youtube">
+
+
+      <div className="left">
+
+      <div className='icon1'><IoMenuOutline /></div>
+
+      <div className='icon'><MdHomeFilled /></div>
+      <p className="left-p">Home</p>
+      <div className='icon'><SiYoutubeshorts /></div>
+      <p className="left-p">Shorts</p>
+      <div className='icon'><MdOutlineSubscriptions /></div>
+      <p className="left-p">Subscription</p>
+      <div className='icon'><MdOutlineVideoLibrary /></div>
+      <p className="left-p">You</p>
+
+      </div>
+
+      <div className="right">
+
+      <div className="nav">
+        <div className="nav1">
+          <div className="icon-nav"><GrYoutube/></div>
+          <p className="p-nav">YouTube</p>
+        </div>
+        <div className="nav2">
+          <input type="search" placeholder="Search"  />
+        </div>
+
+      </div>
+      <div className="content">
+      <div className="row">
+        <div className="gap">
+        <img src={image1} alt="" className='img'/>
+            <div className="tabel-content">
+              <div className="table-left">
+              <p><span className="span1">Telegram Owner ARRESTED 🚨 <br/>Telegram Banned? 🤯</span> <br/>2 days ago</p>
+              </div>
+              <div className="table-right">
+                <HiOutlineDotsVertical/>
+              </div>
+            </div>
+        </div>
+
+        <div className="gap">
+        <img src={image1} alt="" className='img'/>
+            <div className="tabel-content">
+              <div className="table-left">
+              <p><span className="span1">Telegram Owner ARRESTED 🚨 <br/>Telegram Banned? 🤯</span> <br/>2 days ago</p>
+              </div>
+              <div className="table-right">
+                <HiOutlineDotsVertical/>
+              </div>
+            </div>
+        </div>
+
+        <div className="gap">
+        <img src={image1} alt="" className='img'/>
+            <div className="tabel-content">
+              <div className="table-left">
+              <p><span className="span1">Telegram Owner ARRESTED 🚨 <br/>Telegram Banned? 🤯</span> <br/>2 days ago</p>
+              </div>
+              <div className="table-right">
+                <HiOutlineDotsVertical/>
+              </div>
+            </div>
+        </div>
+
+        <div className="gap">
+        <img src={image1} alt="" className='img'/>
+            <div className="tabel-content">
+              <div className="table-left">
+              <p><span className="span1">Telegram Owner ARRESTED 🚨 <br/>Telegram Banned? 🤯</span> <br/>2 days ago</p>
+              </div>
+              <div className="table-right">
+                <HiOutlineDotsVertical/>
+              </div>
+            </div>
+        </div>
+
+      </div>
+
+
+      <div className="row">
+        <div>
+        <img src={image1} alt="" className='img'/>
+            <div className="tabel-content">
+              <div className="table-left">
+              <p><span className="span1">Telegram Owner ARRESTED 🚨 <br/>Telegram Banned? 🤯</span> <br/>2 days ago</p>
+              </div>
+              <div className="table-right">
+                <HiOutlineDotsVertical/>
+              </div>
+            </div>
+        </div>
+
+        <div>
+        <img src={image1} alt="" className='img'/>
+            <div className="tabel-content">
+              <div className="table-left">
+              <p><span className="span1">Telegram Owner ARRESTED 🚨 <br/>Telegram Banned? 🤯</span> <br/>2 days ago</p>
+              </div>
+              <div className="table-right">
+                <HiOutlineDotsVertical/>
+              </div>
+            </div>
+        </div>
+
+        <div>
+        <img src={image1} alt="" className='img'/>
+            <div className="tabel-content">
+              <div className="table-left">
+              <p><span className="span1">Telegram Owner ARRESTED 🚨 <br/>Telegram Banned? 🤯</span> <br/>2 days ago</p>
+              </div>
+              <div className="table-right">
+                <HiOutlineDotsVertical/>
+              </div>
+            </div>
+        </div>
+
+        <div>
+        <img src={image1} alt="" className='img'/>
+            <div className="tabel-content">
+              <div className="table-left">
+              <p><span className="span1">Telegram Owner ARRESTED 🚨 <br/>Telegram Banned? 🤯</span> <br/>2 days ago</p>
+              </div>
+              <div className="table-right">
+                <HiOutlineDotsVertical/>
+              </div>
+            </div>
+        </div>
+
+      </div>
+
+
+
+      <div className="row">
+        <div>
+        <img src={image1} alt="" className='img'/>
+            <div className="tabel-content">
+              <div className="table-left">
+              <p><span className="span1">Telegram Owner ARRESTED 🚨 <br/>Telegram Banned? 🤯</span> <br/>2 days ago</p>
+              </div>
+              <div className="table-right">
+                <HiOutlineDotsVertical/>
+              </div>
+            </div>
+        </div>
+
+        <div>
+        <img src={image1} alt="" className='img'/>
+            <div className="tabel-content">
+              <div className="table-left">
+              <p><span className="span1">Telegram Owner ARRESTED 🚨 <br/>Telegram Banned? 🤯</span> <br/>2 days ago</p>
+              </div>
+              <div className="table-right">
+                <HiOutlineDotsVertical/>
+              </div>
+            </div>
+        </div>
+
+        <div>
+        <img src={image1} alt="" className='img'/>
+            <div className="tabel-content">
+              <div className="table-left">
+              <p><span className="span1">Telegram Owner ARRESTED 🚨 <br/>Telegram Banned? 🤯</span> <br/>2 days ago</p>
+              </div>
+              <div className="table-right">
+                <HiOutlineDotsVertical/>
+              </div>
+            </div>
+        </div>
+
+        <div>
+        <img src={image1} alt="" className='img'/>
+            <div className="tabel-content">
+              <div className="table-left">
+              <p><span className="span1">Telegram Owner ARRESTED 🚨 <br/>Telegram Banned? 🤯</span> <br/>2 days ago</p>
+              </div>
+              <div className="table-right">
+                <HiOutlineDotsVertical/>
+              </div>
+            </div>
+        </div>
+
+      </div>
+
+
+
+      </div>
+      </div>
+
+    </div>
+  )
+}
+
+ReactDOM.render(<Youtube/>,document.getElementById('root'));
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
